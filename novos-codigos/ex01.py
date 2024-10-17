66530e98f91dc0f50b394d5af7fb559fe1d21572
Gelzieny R. Martins <gelzieny@gmail.com>
2024-10-17 17:20:52 -0300

    resolvendo desafio

--- novos-codigos/ex01.py
+++ novos-codigos/ex01.py
@@ -1,1 +1,13 @@
 # Neste algoritmo, crie uma variável que armazene uma string e uma lista que armazena várias strings.
+
+mensagem = input("Digite uma mensagem: ")
+
+lista = []
+variaveis = int(input("Quantas frutas você deseja adicionar à lista? "))
+
+for _ in range(variaveis):
+    info = input("Digite o nome da fruta: ")
+    lista.append(info)
+
+print("Mensagem:", mensagem)
+print("Lista de strings:", lista)
